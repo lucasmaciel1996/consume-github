d1c7d1a6b608390d744d6b9006d4e2104a045b96
Lucas Maciel <lucas333maciel@gmail.com>
2019-09-28 17:45:15 -0300

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Consume API GitHub</h1>
+    <strong>Lucas Maciel</strong>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
